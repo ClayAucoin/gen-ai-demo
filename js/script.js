3c653dcb74cde0d2e9695d52cf40bece86d61733
ClayAucoin <clay0aucoin@gmail.com>
2025-09-03 18:12:39 -0500

    UI and DOM wiring completed

--- js/script.js
+++ js/script.js
@@ -2,32 +2,34 @@ console.log("script.js file says hello.")
 
 onEvent("sendButton", "click", function () {
 
-    async function query(data) {
-        const response = await fetch(
-            "https://router.huggingface.co/v1/chat/completions",
-            {
-                headers: {
-                    Authorization: `Bearer ${HF_TOKEN}`,
-                    "Content-Type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify(data),
-            }
-        );
-        const result = await response.json();
-        return result;
-    }
+    setText("aiResponse", getValue("userInput"));
 
-    query({
-        messages: [
-            {
-                role: "user",
-                content: "What is the capital of France?",
-            },
-        ],
-        model: "meta-llama/Llama-3.3-70B-Instruct:fireworks-ai",
-    }).then((response) => {
-        console.log(JSON.stringify(response));
-    });
+    // async function query(data) {
+    //     const response = await fetch(
+    //         "https://router.huggingface.co/v1/chat/completions",
+    //         {
+    //             headers: {
+    //                 Authorization: `Bearer ${HF_TOKEN}`,
+    //                 "Content-Type": "application/json",
+    //             },
+    //             method: "POST",
+    //             body: JSON.stringify(data),
+    //         }
+    //     );
+    //     const result = await response.json();
+    //     return result;
+    // }
+
+    // query({
+    //     messages: [
+    //         {
+    //             role: "user",
+    //             content: "What is the capital of France?",
+    //         },
+    //     ],
+    //     model: "meta-llama/Llama-3.3-70B-Instruct:fireworks-ai",
+    // }).then((response) => {
+    //     console.log(JSON.stringify(response));
+    // });
 
 });
