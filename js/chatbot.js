56469a73f63fb7d3670c575f4d6f8b4fd5c8e1bd
ClayAucoin <clay0aucoin@gmail.com>
2025-09-04 02:14:40 -0500

    finished validation

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,25 +1,51 @@
 console.log("chatbot.js file says hello.")
 
+/*
+    REMEMBER TO CHANGE TO ACTIVE FUNCTION AFTER EVERYTHING WORKS
+    AND REMOVE VALUE FROM INPUT ON INDEX
+
+    What is the capital of Louisiana?
+*/
+
 setValue("userInput", "");
+changePlaceholder("Your sentence here...");
 
 let botReply = "";
 let userInput = "";
+
 
 onEvent("sendButton", "click", function () {
 
     userInput = getValue("userInput")
 
     if (userInput) {
-        sendToModel();
+        removeClass("userInput", "error-placeholder");
+        thinking();
+        setText("aiResponse", "Thinking . . .");
+        // sendToModel();
+        console.log("userInput: " + userInput);
     } else {
-        alert("Please enter a question.");
+        //setStyle("userInput", "border: 2px solid red;");
+        addClass("userInput", "red-border")
+        addClass("userInput", "error-placeholder")
+        changePlaceholder("Please enter a question.");
+        setTimeout(() => {
+            alert("Please enter a question.");
+        }, 50);
     }
 });
+
+    // remove input field styling when user starts typing
+document.getElementById("userInput").addEventListener("input", () => {
+    removeClass("userInput", "error-placeholder");
+    removeClass("userInput", "red-border");
+});
+
 
 function sendToModel() {
     console.log("send to model called");
 
-    setText("aiResponse", "Thinking...");
+    setStyle("aiResponse", "font-style: normal;");
 
     async function query(data) {
         const response = await fetch(
@@ -51,4 +77,10 @@ function sendToModel() {
         setText("aiResponse", botReply);
     });
 
+}
+
+
+
+function changePlaceholder(string) {
+    document.getElementById("userInput").placeholder = string;
 }
